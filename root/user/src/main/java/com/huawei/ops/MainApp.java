ce5fb78ff97b86a143fb1cc97362f5605efdaa38
qixingbeidou <zhanjun.zhang@qq.com>
2017-07-01 17:23:15 +0800

    code frame.

---
+++ root/user/src/main/java/com/huawei/ops/MainApp.java
@@ -0,0 +1,13 @@
+package com.huawei.ops;
+
+
+/**
+ * Created by Administrator on 2017/6/4.
+ */
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("hello gradle!");
+    }
+}
